a1a27e04df91731292132f9709e471c5f8dda95d
Casey <dinnerdoggy@gmail.com>
2025-05-20 21:06:54 -0500

    added seed data

--- Program.cs
+++ Program.cs
@@ -1,4 +1,10 @@
+using Microsoft.EntityFrameworkCore;
+using Taskblaster.Data;
+
 var builder = WebApplication.CreateBuilder(args);
+
+var connectionString = builder.Configuration.GetConnectionString("TaskBlasterDbConnectionString");
+builder.Services.AddDbContext<TaskBlasterDbContext>(options => options.UseNpgsql(connectionString));
 
 // Add services to the container.
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
