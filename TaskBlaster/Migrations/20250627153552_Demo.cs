bf935d7a7aa12e7432735a19afbdb4c28e3a8063
Casey <dinnerdoggy@gmail.com>
2025-06-29 18:10:29 -0500

    preparing for deployment

---
+++ TaskBlaster/Migrations/20250627153552_Demo.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace TaskBlaster.Migrations
+{
+    /// <inheritdoc />
+    public partial class Demo : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
